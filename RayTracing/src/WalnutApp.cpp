949fd70593a0633af73ccc8259013aaf5b415977
sik1j <karkisikij2@gmail.com>
2022-09-01 14:14:54 -0700

    Displaying a static color magenta image

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -1,5 +1,6 @@
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
+#include <iostream>
 
 #include "Walnut/Image.h"
 
@@ -9,11 +10,44 @@ public:
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		if (ImGui::Button("Render"))
+			Render();
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::Begin("Viewport");
+		viewportWidth = ImGui::GetContentRegionAvail().x;
+		viewportHeight = ImGui::GetContentRegionAvail().y;
+		if(m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), {(float)viewportWidth, (float)viewportHeight});
+		ImGui::End();
+
 	}
+
+	void Render()
+	{
+		if (!m_Image)
+		{
+			m_Image = std::make_shared<Walnut::Image>(viewportWidth, viewportHeight, Walnut::ImageFormat::RGBA);
+		}
+
+		delete[] m_pImageData;
+		m_pImageData = new uint32_t[viewportHeight * viewportWidth];
+		for (uint32_t i = 0; i < viewportHeight * viewportWidth; i++)
+		{
+			m_pImageData[i] = 0xffff00ff;
+		}
+
+		std::cout << m_pImageData[0];
+		m_Image->SetData(m_pImageData);
+	}
+
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
+
+	uint32_t viewportWidth;
+	uint32_t viewportHeight;
+	uint32_t* m_pImageData = nullptr;
+
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
