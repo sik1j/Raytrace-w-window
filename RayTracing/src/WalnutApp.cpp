af14b6b66ac8b9e606010f1dc091e82107a68810
sik1j <karkisikij2@gmail.com>
2022-09-01 14:44:13 -0700

    Display image based on image dimensions instead of viewport dimensions

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -11,41 +11,56 @@ public:
 	{
 		ImGui::Begin("Hello");
 		if (ImGui::Button("Render"))
+		{
 			Render();
+		}
 		ImGui::End();
 
 		ImGui::Begin("Viewport");
-		viewportWidth = ImGui::GetContentRegionAvail().x;
-		viewportHeight = ImGui::GetContentRegionAvail().y;
-		if(m_Image)
-			ImGui::Image(m_Image->GetDescriptorSet(), {(float)viewportWidth, (float)viewportHeight});
+
+		m_viewportWidth = ImGui::GetContentRegionAvail().x;
+		m_viewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if (m_Image)
+		{
+			// ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_viewportWidth, (float)m_viewportHeight });
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
+		}
 		ImGui::End();
 
 	}
 
 	void Render()
 	{
-		if (!m_Image)
+		std::cout << "rendered with W: " << m_viewportWidth << " and H: " << m_viewportHeight << "\n";
+		if (!m_Image || m_Image->GetHeight() != m_viewportHeight || m_Image->GetWidth() != m_viewportWidth)
 		{
-			m_Image = std::make_shared<Walnut::Image>(viewportWidth, viewportHeight, Walnut::ImageFormat::RGBA);
-		}
-
+			m_Image = std::make_shared<Walnut::Image>(m_viewportWidth, m_viewportHeight, Walnut::ImageFormat::RGBA);
 		delete[] m_pImageData;
-		m_pImageData = new uint32_t[viewportHeight * viewportWidth];
-		for (uint32_t i = 0; i < viewportHeight * viewportWidth; i++)
-		{
-			m_pImageData[i] = 0xffff00ff;
+		m_pImageData = new uint32_t[m_viewportHeight * m_viewportWidth];
 		}
 
-		std::cout << m_pImageData[0];
+		for (uint32_t i = 0; i < m_viewportHeight * m_viewportWidth; i++)
+		{
+			if(i < (m_viewportHeight * m_viewportWidth) / 4 )
+			{ 
+				m_pImageData[i] = 0xffff00ff;
+			}
+			else if (i < (2*(m_viewportHeight * m_viewportWidth)/4))
+			{
+			}
+			else
+			{ 
+				m_pImageData[i] = 0xffff0000;
+			}
+		}
+
 		m_Image->SetData(m_pImageData);
 	}
 
 private:
 	std::shared_ptr<Walnut::Image> m_Image;
-
-	uint32_t viewportWidth;
-	uint32_t viewportHeight;
+	uint32_t m_viewportWidth = 0, m_viewportHeight = 0;
 	uint32_t* m_pImageData = nullptr;
 
 };
